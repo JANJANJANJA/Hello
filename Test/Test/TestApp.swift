4ff78218bf5163cec8e80267ffca86f3ee809d9a
Jaein Yi <jaeinyi@Jaeinui-MacBookAir.local>
2022-02-04 16:06:39 +0900

    Test

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Jaein Yi on 2022/02/04.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
